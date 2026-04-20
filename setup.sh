366f3799cf022f84418417cbe479d8884f68f517
RVidushikaR <vrathnam@purdue.edu>
2026-04-19 22:52:48 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,16 @@ conda env create -f environment.yml
 echo "Activating environment..."
 source activate llama_ui
 
+echo "Setting up Node project..."
+
+# Only create package.json if it doesn't exist
+if [ ! -f package.json ]; then
+    npm init -y
+fi
+
+# Add ES module support (THIS FIXES YOUR WARNING)
+npm pkg set type=module
+
 echo "Installing Node dependencies..."
 npm install express cors
 
